ad7c28f78ad71a060432e75c37c715d94b5b57b0
VishalAmbavade <vaambavade@gmail.com>
2022-11-13 19:10:54 +0000

    Assignment 6 solution

--- temp_ass6.R
+++ temp_ass6.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+
 sample_x_0310 <- function(alpha, beta, n) {
   df <- data.frame(U = runif(n)) %>%
     mutate(X = case_when((0 <= U) & (U < alpha) ~ 3,
@@ -9,15 +14,42 @@ sample_x_0310 <- function(alpha, beta, n) {
 
 
 temp <- data.frame(beta = seq(1/10, 9/10, 0.01))
-vals <- c(3, 10, 0)
-probs <- c(1/10, i, (1 - 1/10 - i))
 
+newEx <- c()
+for (i in myData$beta) {
+  vals <- c(3, 10, 0)
+  probs <- c(1/10, i, 1 - (0.1 + i))
+  Ex <- weighted.mean(vals, probs)
+  newEx <- c(newEx, Ex)
+}
+
+vals <- c(3, 10, 0)
+probs <- c(1/2, 1/10, 2/5)
+Ex <- weighted.mean(vals, probs)
+Ex
+
+Ex_df <- data.frame(myData$beta * Ex)
 
 myData <- temp %>%
   mutate(sample_x = map(.x = beta, ~sample_x_0310(1/10, .x, 100)),
-         samplemean = mean(sample_x))
+         samplemean = map_dbl(.x = sample_x, ~mean(.x)),
+         #probs = map(.x = beta, ~rep(.x, times = 100)),
+         Expectation = newEx)
+         #sumCol = map_dbl(.x = sample_x, ~sum(.x)))
+         #Expectation = weighted.mean(sample_x, probs))
+
+
+         #Expectation = map_dbl(.x = beta, ~weighted.mean(sample_x, probs)))
+
+zzz <- myData %>%
+  unlist(sample_x)
   #sample_x <- sample_x_0310(1/10, i, 100)
   #samplemean = mean(sample_x)
   #Expectation = weighted.mean(vals, probs)
-} 
+
+d <- gather(myData, key = "attr", value = "value", Expectation, samplemean)
+
+ggplot(d, aes(x = beta, y = value, color = attr)) +
+  geom_point()
+
 
